194644c5dadcbc50864a01319c04b94b65958da9
shinmyoungha <audgk0309@naver.com>
2025-06-19 20:24:35 +0900

    06/19 myungha

--- catchwork-react/src/App.jsx
+++ catchwork-react/src/App.jsx
@@ -1,20 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
-import MainPage from './pages/major/MainPage'
-import Header from './components/common/Header'
-import Footer from './components/common/Footer'
+import WriteSupportPage from "./pages/support/WriteSupportPage";
+import logo from "./assets/logo.png"; // 실제 logo.png 경로
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/catchwork" element={<MainPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
+    <div>
+      <WriteSupportPage logo={logo} />
+    </div>
   );
 }
 
