9bb095d3ad1f6fff81c45f3e91b783e6e9df7d27
JIN325JIN <wavyfromtheocean@gmail.com>
2025-06-20 11:44:33 +0900

    06/20 submitcv&companylist

--- catchwork-react/src/AppRoute.jsx
+++ catchwork-react/src/AppRoute.jsx
@@ -1,23 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import NotFound from './pages/error/NotFound'
+import NotFound from "./pages/error/NotFound";
 
-import MainPage from './pages/major/MainPage'
-import SignInPage from './pages/member/SignInPage'
-import SignUpPage from './pages/member/SignUpPage'
+import MainPage from "./pages/major/MainPage";
+import SignInPage from "./pages/member/SignInPage";
+import SignUpPage from "./pages/member/SignUpPage";
 
-import RecruitPage from './pages/major/RecruitListPage'
-import CompanyPage from './pages/major/CompanyListPage'
-import BoardPage from './pages/major/BoardListPage'
-import CvPage from './pages/major/CVListPage'
+import RecruitPage from "./pages/major/RecruitListPage";
+import CompanyPage from "./pages/major/CompanyListPage";
+import BoardPage from "./pages/major/BoardListPage";
+import CvPage from "./pages/major/CVListPage";
+
+import MyPage from "./pages/myPage/MyPage";
+
+import SubmitCvPage from "./pages/major/SubmitCvPage";
 
 function AppRoute() {
   return (
     <>
       <main>
         <Routes>
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/" element={<MainPage />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/" element={<MainPage />} />
 
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
@@ -26,10 +30,15 @@ function AppRoute() {
           <Route path="/company" element={<CompanyPage />} />
           <Route path="/board" element={<BoardPage />} />
           <Route path="/cv" element={<CvPage />} />
+
+          <Route path="/mypage" element={<MyPage />} />
+
+          {/*윤진*/}
+          <Route path="/submitcvpage" element={<SubmitCvPage />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default AppRoute
+export default AppRoute;
--- catchwork-react/src/pages/major/CompanyListPage.jsx
+++ catchwork-react/src/pages/major/CompanyListPage.jsx
@@ -1,14 +1,84 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import SectionHeader from "../../components/common/SectionHeader";
+import CompanyCard from "../../components/common/CompanyCard";
+import "./CompanyListPage.css";
 
 const CompanyListPage = () => {
+  const [companyList, setCompanyList] = useState([
+    {
+      // 가데이터 넣어서 확인하는중임
+      corpRegNo: 1,
+      corpName: "KH 정보교육원 종로",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    {
+      corpRegNo: 2,
+      corpName: "KH 정보교육원 강남",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    {
+      corpRegNo: 3,
+      corpName: "KH 정보교육원 의정부",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    {
+      corpRegNo: 4,
+      corpName: "KH 정보교육원 인천",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    {
+      corpRegNo: 5,
+      corpName: "KH 정보교육원 신촌",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    {
+      corpRegNo: 6,
+      corpName: "KH 정보교육원 노원",
+      corpType: "중소기업",
+      corpLogo: "/src/assets/icon.png",
+      views: 120,
+      favs: 70,
+    },
+    // ... 더미 데이터 추가 가능
+  ]);
+
   return (
-    <div className="companylist-page">
-        <h1>CompanyListPage</h1>
-        <h1>CompanyListPage</h1>
-        <h1>CompanyListPage</h1>
-        <h1>CompanyListPage</h1>
-        <h1>CompanyListPage</h1>
-    </div>
+    <>
+      <main className="container">
+        <div className="section-header-with-search">
+          <SectionHeader title="기업정보" />
+
+          <div className="search-box">
+            <button>검색</button>
+            <input type="text" placeholder="진정한 취업이야기, 취준진담" />
+          </div>
+        </div>
+
+        {/* 기업 카드 리스트 */}
+        <div className="company-grid">
+          {companyList.map((company) => (
+            <CompanyCard key={company.corpRegNo} company={company} />
+          ))}
+        </div>
+      </main>
+    </>
   );
 };
 
