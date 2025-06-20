bb493437df436efe756b218db1896f83f6e1187e
ITJANG <whalswkd1213@gmail.com>
2025-06-20 11:45:02 +0900

    edit appRouter&MyPage&MyInfo&SideBar

--- catchwork-react/src/AppRoute.jsx
+++ catchwork-react/src/AppRoute.jsx
@@ -1,23 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import NotFound from './pages/error/NotFound'
+import NotFound from "./pages/error/NotFound";
 
-import MainPage from './pages/major/MainPage'
-import SignInPage from './pages/member/SignInPage'
-import SignUpPage from './pages/member/SignUpPage'
+import MainPage from "./pages/major/MainPage";
+import SignInPage from "./pages/member/SignInPage";
+import SignUpPage from "./pages/member/SignUpPage";
 
-import RecruitPage from './pages/major/RecruitListPage'
-import CompanyPage from './pages/major/CompanyListPage'
-import BoardPage from './pages/major/BoardListPage'
-import CvPage from './pages/major/CVListPage'
+import RecruitPage from "./pages/major/RecruitListPage";
+import CompanyPage from "./pages/major/CompanyListPage";
+import BoardPage from "./pages/major/BoardListPage";
+import CvPage from "./pages/major/CVListPage";
+import MyPage from "./pages/myPage/MyPage";
+import MyInfoPage from "./pages/myPage/MyInfoPage";
+import MyRecruitPage from "./pages/myPage/MyRecruitPage";
 
 function AppRoute() {
+  const myPageRoutes = {
+    path: "/mypage",
+    element: <MyPage />,
+    children: [
+      { path: "", element: <MyInfoPage /> },
+      { path: "myrecruit", element: <MyRecruitPage /> },
+    ],
+  };
+
   return (
     <>
       <main>
         <Routes>
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/" element={<MainPage />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/" element={<MainPage />} />
 
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
@@ -26,10 +38,15 @@ function AppRoute() {
           <Route path="/company" element={<CompanyPage />} />
           <Route path="/board" element={<BoardPage />} />
           <Route path="/cv" element={<CvPage />} />
+
+          <Route path="/mypage" element={<MyPage />}>
+            <Route path="home" element={<MyInfoPage />} />
+            <Route path="myrecruit" element={<MyRecruitPage />} />
+          </Route>
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default AppRoute
+export default AppRoute;
