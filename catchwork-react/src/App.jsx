b067acfd0d6546e7d1f565380d8818f399de7372
JIN325JIN <wavyfromtheocean@gmail.com>
2025-06-20 17:07:13 +0900

    06/20 root color rename & company &companydetail

--- catchwork-react/src/App.jsx
+++ catchwork-react/src/App.jsx
@@ -1,49 +1,56 @@
 import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
- // 레이아웃
-import Layout from './Layout';
+// 레이아웃
+import Layout from "./Layout";
 
 // 오류 및 메인
-import NotFound from './pages/error/NotFound'
-import MainPage from './pages/major/MainPage'
+import NotFound from "./pages/error/NotFound";
+import MainPage from "./pages/major/MainPage";
 
 // 회원
-import SignInPage from './pages/member/SignInPage'
-import SignUpPage from './pages/member/SignUpPage'
+import SignInPage from "./pages/member/SignInPage";
+import SignUpPage from "./pages/member/SignUpPage";
 
 // 네비게이션
-import RecruitPage from './pages/major/RecruitListPage'
-import CompanyPage from './pages/major/CompanyListPage'
-import BoardPage from './pages/major/BoardListPage'
-import CvPage from './pages/major/CVListPage'
+import RecruitPage from "./pages/major/RecruitListPage";
+import CompanyPage from "./pages/major/CompanyListPage";
+import BoardPage from "./pages/major/BoardListPage";
+import CVPage from "./pages/major/CVListPage";
 
 // 마이페이지용용
-import MyPage from './pages/myPage/MyPage'
+import MyPage from "./pages/myPage/MyPage";
 import MyInfo from "./pages/myPage/MyInfoPage";
+
+//이력서 제출
+import SubmitCVPage from "./pages/major/SubmitCVPage";
+
+// 기업상세
+import CompanyDetailPage from "./pages/major/CompanyDetailPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       { index: true, element: <MainPage /> },
-      { path: 'signin', element: <SignInPage /> },
-      { path: 'signup', element: <SignUpPage /> },
-      { path: 'recruit', element: <RecruitPage /> },
-      { path: 'company', element: <CompanyPage /> },
-      { path: '/board', element: <BoardPage /> },
-      { path: 'cv', element: <CvPage />, },
-
+      { path: "signin", element: <SignInPage /> },
+      { path: "signup", element: <SignUpPage /> },
+      { path: "recruit", element: <RecruitPage /> },
+      { path: "company", element: <CompanyPage /> },
+      { path: "company/:corpNo", element: <CompanyDetailPage /> },
+      { path: "/board", element: <BoardPage /> },
+      { path: "cv", element: <CVPage /> },
+      { path: "submitcv", element: <SubmitCVPage /> },
 
       {
-        path: '/mypage',
+        path: "/mypage",
         element: <MyPage />, // 공통 wrapper 컴포넌트
         children: [
-          { path: 'home', element: <MyInfo /> }, // 로컬호스트/mypage/home
-        ]
+          { path: "home", element: <MyInfo /> }, // 로컬호스트/mypage/home
+        ],
       },
-      { path: '*', element: <NotFound /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
