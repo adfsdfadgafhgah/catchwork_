bfbea12695c418311c42a1f1c93ca66bd46de698
ITJANG <whalswkd1213@gmail.com>
2025-06-26 18:18:58 +0900

    아임 설희

--- catchwork-spring/catchwork/src/main/java/com/example/demo/common/config/WebConfig.java
+++ catchwork-spring/catchwork/src/main/java/com/example/demo/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
