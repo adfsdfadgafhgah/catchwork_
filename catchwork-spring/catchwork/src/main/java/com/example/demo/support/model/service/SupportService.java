3eb92e13142829f0b9ab3413c8311cbdc983ff80
shinmyoungha <audgk0309@naver.com>
2025-06-27 17:21:32 +0900

    06/27 SupportListPage, SupportDetailPage 서버랑 연결했습니다

---
+++ catchwork-spring/catchwork/src/main/java/com/example/demo/support/model/service/SupportService.java
@@ -0,0 +1,15 @@
+package com.example.demo.support.model.service;
+
+import java.util.List;
+import com.example.demo.support.model.dto.Support;
+
+public interface SupportService {
+	
+	// 문의하기 리스트
+    List<Support> getSupportList();
+
+    // 문의하기 디테일 아이디로 찾기
+    Support findById(int id);
+    
+
+}
