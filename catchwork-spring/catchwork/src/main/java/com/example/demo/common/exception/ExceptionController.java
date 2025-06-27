62419c66424721e09ccc3b09dc7aac6d9d45d684
JIN325JIN <wavyfromtheocean@gmail.com>
2025-06-27 17:19:46 +0900

    06/27 sts4 - corp page & DB no dummy data

--- catchwork-spring/catchwork/src/main/java/com/example/demo/common/exception/ExceptionController.java
+++ catchwork-spring/catchwork/src/main/java/com/example/demo/common/exception/ExceptionController.java
@@ -1,12 +1,12 @@
 package com.example.demo.common.exception;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 
-@ControllerAdvice // 전역적 예외처리 활성화 어노테이션
+@RestControllerAdvice // 전역적 예외처리 활성화 어노테이션
 public class ExceptionController {
 	
 	@ExceptionHandler(NoResourceFoundException.class)
