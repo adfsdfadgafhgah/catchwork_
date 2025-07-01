eb38da3d269a41b025c158bf8e67f5ca611146eb
shinmyoungha <audgk0309@naver.com>
2025-07-01 09:14:23 +0900

    07/01 문의하기 리스트 문의하기 작성하기 문의하기 디테일 수정하고 있습니다

--- catchwork-spring/catchwork/src/main/java/com/example/demo/support/model/mapper/SupportMapper.java
+++ catchwork-spring/catchwork/src/main/java/com/example/demo/support/model/mapper/SupportMapper.java
@@ -9,12 +9,26 @@ import org.apache.ibatis.annotations.Param;
 public interface SupportMapper {
 	
 	// 문의하기 리스트
+    /**
+     * @author 명하
+     * @return
+     */
     List<Support> selectSupportList();
     
     // 문의하기 디테일 아이디로 찾기
+    /**
+     * @author 명하
+     * @param id
+     * @return
+     */
     Support selectSupportById(@Param("id") int id);
     
     // 문의하기 작성
+    /**
+     * @author 명하
+     * @param support
+     * @return
+     */
     int insertSupport(Support support);
 }
 
