989875acf07011fe17e748a501a226551462295d
kichanwon <wgichan@gmail.com>
2025-07-08 16:17:52 +0900

    07/08 merge

--- catchwork-spring/catchwork/src/main/java/com/example/demo/corp/company/model/mapper/CorpInfoMapper.java
+++ catchwork-spring/catchwork/src/main/java/com/example/demo/corp/company/model/mapper/CorpInfoMapper.java
@@ -1,6 +1,5 @@
 package com.example.demo.corp.company.model.mapper;
 
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,34 +7,31 @@ import com.example.demo.corp.company.model.dto.CorpInfo;
 
 @Mapper
 public interface CorpInfoMapper {
-	
-		/** 공고 작성페이지에서 기업정보 가져오기
-		 * @author BAEBAE
-		 * @param memNo
-		 * @return
-		 */
-		CorpInfo getCorpInfoByMember(String memNo);
-	
-		/** memNo 기반
-		 * 기업회원이 본인의 기업 상세를 보는
-		 * @author JIN
-		 * @param memNo
-		 * @return
-		 */
-		CorpInfo selectCorpByMemNo(@Param("memNo") String memNo);
 
-		/** 기업 정보 수정
-		 * @author JIN
-		 * @param corpInfo
-		 * @return
-		 */
-		int updateCorpInfo(CorpInfo corpInfo);
+	/**
+	 * memNo 기반 기업회원이 본인의 기업 상세를 보는
+	 * 
+	 * @author JIN
+	 * @param memNo
+	 * @return
+	 */
+	CorpInfo selectCorpByMemNo(@Param("memNo") String memNo);
 
-		/** 공고 작성페이지에서 기업정보 가져오기
-		 * @param memNo
-		 * @return
-		 */
-		CorpInfo selectCorpInfoByMember(String memNo);
+	/**
+	 * 기업 정보 수정
+	 * 
+	 * @author JIN
+	 * @param corpInfo
+	 * @return
+	 */
+	int updateCorpInfo(CorpInfo corpInfo);
 
-	    
+	/**
+	 * 공고 작성페이지에서 기업정보 가져오기
+	 * 
+	 * @param memNo
+	 * @return
+	 */
+	CorpInfo selectCorpInfoByMember(String memNo);
+
 }
